c115fa13f123dccb824666dfd84efda71f8514aa
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-10-09 03:11:16 +0530

    Updated as an optimized solution

--- Binary_search.cpp
+++ Binary_search.cpp
@@ -2,35 +2,14 @@
 using namespace std;
 
 
-int bin(int ans[], int n, int k) {
-
-	int start = 0;
-	int end = n - 1;
-	int mid = (start + end) / 2;
-
-	while (start <= end) {
-
-		if (ans[mid] == k) {
-			return mid;
-
-		}
-		if (ans[mid] > k) {
-			end = mid - 1;
-		}
-		if (ans[mid] < k) {
-			start = mid + 1;
-		}
-
-		mid = (start + end) / 2;
-
-
-
-
-	}
-	return -1;
-
-
+bool search(int x[], int n, int k) {
+    int p = 0;
+    for (int a = n; a >= 1; a /= 2) {
+        while (p+a < n && x[p+a] <= k) p += a;
+    }
+    return x[p] == k;
 }
+
 int main() {
 
 
